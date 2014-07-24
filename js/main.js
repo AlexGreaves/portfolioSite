401440f612d370bbb0ec9c09f4396bb343cffef4
Alex <holla@alexgreaves.com>
2014-07-24 16:26:39 +0100

    all work imgs,swf

--- js/main.js
+++ js/main.js
@@ -1,113 +1,14 @@
 var fn = {
 
 	content:null,
-	myUrl: window.location.host,
+	hostUrl: 'localhost',
 	currentUrl:'',
 	previousUrl:'',
 	main: $('#main'),
 
-	
-
 	init: function(data) {
 		fn.content = data;
 		fn.linking();
-	},
-
-	checkURL:function(){
-		fn.currentUrl = cleanURL();
-		var pageToLoad;
-		var modal = '';
-		// trace('current url length is '+currentUrl.length);
-		// trace(currentUrl);
-		
-		if (fn.currentUrl[1]){
-			$.each(fn.content, function (index, section){
-				var str = section.href.toString();
-				if (fn.currentUrl[1] == (str) && str != 'home-page'){
-					// trace(pageToLoad);
-					if (fn.currentUrl[2]){
-						// trace(fn.currentUrl[2]);
-						$.each(section.main.thumbnailImages, function (index, section){
-							str = section.href.toString().split('/');
-							var subStr = str[1];
-							if (fn.currentUrl[2] == subStr){
-								modal = section;
-								trace ('theres a winning modal, display it');
-								// pageToLoad = [section, section, section.format];
-								// $('.modal').removeClass('hide');
-								return false;
-							}
-						});
-						// check through section thumbnails and return modal yes
-					}
-					pageToLoad = [section, section.main.thumbnailImages,modal];
-					return false;
-				} else {
-					pageToLoad = [fn.content.homePage, fn.content,''];		
-				}
-			});
-		} else {
-			pageToLoad = [fn.content.homePage, fn.content,''];
-		}
-		return pageToLoad;
-	},
-
-	updateContent:function(a,initialState){
-		var $main = $("#main");
-		var $a = a[0];
-		var $b = a[1];
-		var $modal = a[2];
-
-		if(fn.currentUrl.length < 3 && initialState == 'initial'){
-			$('body').fadeOut(0, function(){
-				$main.empty().append(fn.loadMarkup($a));
-				// $('body').removeClass('hide');
-				$('body').fadeIn(250)
-				$('body').ScrollTo();
-				fn.loadWork($b);
-			});
-		} else if (fn.currentUrl.length == 3 && initialState == 'initial'){
-			$('body').fadeOut(0, function(){
-				$main.empty().append(fn.loadMarkup($a));
-				// $('body').removeClass('hide');
-				$('body').fadeIn(250);
-				$('body').ScrollTo();
-				fn.loadWork($b);
-				trace($modal);
-				//load the required modal
-				showModal($modal);
-				// $('.modal').removeClass('hide');
-			});
-		};
-
-		if (initialState != 'initial' && fn.previousUrl.length < 3 && fn.currentUrl.length < 3){
-			$main.fadeOut(250, function(){
-				$main.empty().append(fn.loadMarkup($a)).fadeIn(250);
-				$('body').ScrollTo();
-				fn.loadWork($b);
-			});
-		} else if (initialState != 'initial' && fn.currentUrl.length == 3){
-			trace('modal to fade in');
-			showModal();
-		}
-
-		if (fn.previousUrl.length == 3) {
-			$('.modal').removeClass('show');
-			$('.modal').empty()
-		}
-
-		function showModal(){
-			
-			$('.modal').append(fn.loadMarkup($modal));
-
-			$('.modal').addClass('show');
-			$('.modal-content').ScrollTo();
-			fn.addThis($modal);
-			
-			// $('body').addClass('open-modal');
-			
-		}
-		fn.previousUrl = cleanURL(window.location.href);
 	},
 
 	addThis:function(modal){
@@ -123,49 +24,187 @@ var fn = {
 			var State = History.getState(); // Note: We are using History.getState() instead of event.state 
 			History.replaceState('', '', State.urlPath);
 
-			fn.updateContent(fn.checkURL(),'initial');
+			fn.updateContent(fn.checkUrl(),'true');
 		} else {
 			return false;
 		}
 
 		History.Adapter.bind(window, 'statechange', function(){
-			fn.updateContent(fn.checkURL(),'');
+			fn.updateContent(fn.checkUrl(),'');
 		});
 
-	    $(document).on("click", ".tile", function(e) {
+	    $(document).on("click", ".tile ", function(e) {
 	    	// this will have to be different for the type of tile
-	    	var $a = $(this).data('content');
-	    	e.preventDefault();
-	    	History.pushState('', '', $a.href);
+	    	if(!$(this).hasClass('locked')){
+	    		var $a = $(this).data('content');
+		    	// trace($a);
+		    	e.preventDefault();
+		    	History.pushState('', '', $a.href);	
+	    	} else {
+	    		trace('this content is locked');
+	    	}
+	    	
 	    });
-	    $(document).on("click", ".modal", function(e){
-	    	trace('click');
+
+	    $('.homeUrl').on("click", function() {
+	    	History.pushState('', '', ' ');
+	    });
+
+	    $(document).on("click", ".close, .modal", function(e){
+
 	    	e.preventDefault();
-	    	var closeLocation = (cleanURL(window.location.href));
-	    	var newUrl ='/'+closeLocation[1];
-	    	History.pushState('', '', newUrl);
-	    	// History.back();
+	    	var a = window.location.href;
+	    	var b = a.lastIndexOf('/');
+	    	var c = a.substring(b, a.length);
+	    	var d = a.split(c).join('');
+	    	var newUrl = d;
+    		History.pushState('', '', newUrl);	
+	    	
 	    });
 	    $(document).on("click", ".modal-content", function(e){
 	    	e.stopPropagation();
 	    });
+	    $(document).on("click", ".banner", function(e){
+	    	var $a = $(this).data('content');
+	    	var $width = $(this).children([0]).width(); 
+	    	var $height = $(this).children([0]).height();
+	    	var $replace = $(this).find($('.replace-me'));
+	    	$replace.flash({src:$a.swf, height:$height, width:$width});
+	    });
+	    $(document).on("click", "button", function(e){
+	    	window.location.href = $(this).attr('href');
+	    });
+	    
+	},
+		// add check url and update content
+	checkUrl:function(){
+		fn.currentUrl = cleanURL(window.location.href);
+		var markup = '';
+		var tiles = '';
+		var modal = '';
+
+		var pageToLoad; // this is an object containing the information for update content
+		trace(fn.currentUrl);
+		if (fn.currentUrl[1] && fn.currentUrl[1] != 'home-page' && fn.currentUrl[1] != 'index'){
+			$.each(fn.content, function (index, section){
+				if (section.href == fn.currentUrl[1]){
+					trace(section.href);
+					if(section.format == 'modal'){
+						modal = section;
+						markup = fn.content.homePage;
+						tiles = fn.content;
+					} else if(section.format == 'skill'){
+						markup = section;
+						tiles = section.main.thumbnailImages;
+
+							if (fn.currentUrl.length > 2){
+								$.each(section.main.thumbnailImages, function (index, section){
+									str = section.href.toString().split('/');
+									subStr = str[str.length-1];
+									if (fn.currentUrl[2] == subStr){
+										modal = section;
+										return false;
+									}
+							});	
+						}
+						
+					}
+					return false;
+				}
+				// else {
+				// 	trace('404 page');
+				// 	markup = section;
+				// 	// tiles = '';	
+				// 	return false;
+				// }
+			});
+		} else {
+			markup = fn.content.homePage;
+			tiles = fn.content;
+		}
+
+		pageToLoad = [markup,tiles,modal];
+		return pageToLoad; // always return object	
 	},
 
+	updateContent:function(toLoad,initialState){
+		var $main = $('#main')
+		var markup = toLoad[0];
+		var tiles = toLoad[1];
+		var modal = toLoad[2];
+
+		// load page based on initial state
+
+		if (initialState == 'true') {
+			//check to see if this is the first load to show body
+			$('body').fadeOut(0, function(){
+				$main.empty().append(fn.loadMarkup(markup)).fadeIn(250);
+				$('body').ScrollTo();
+				
+				if(tiles != ''){
+					fn.loadWork(tiles);
+					trace(tiles);	
+				}
+
+				$('body').removeClass('hide');
+				$('body').fadeIn(250);
+			});
+
+		} else {
+
+			if(modal != '' || $('.modal').hasClass('show')){
+				
+			} else{
+				$main.fadeOut(250, function(){
+					$main.empty().append(fn.loadMarkup(markup)).fadeIn(250);
+					$('body').ScrollTo();
+					fn.loadWork(tiles);
+				});	
+			}
+			
+
+		}
+
+		// load the modal if there is one
+
+		if (modal != ''){
+
+			$('.modal').empty().append(fn.loadMarkup(modal)).fadeIn(250);
+			fn.loadRichContent(modal);	
+			fn.addThis(modal);
+			$('.modal').addClass('show');
+			$('.modal-content').ScrollTo();
+
+		} else {
+
+			$('.modal').removeClass('show');
+
+			// show or hide back button
+			if(fn.currentUrl[1] != '' && fn.currentUrl[1] != 'home-page'){
+				$('.back-holder').removeClass('hide');
+			} else if ($('.back-holder').hasClass('hide') != true){
+				$('.back-holder').addClass('hide');
+			}
+
+		}
+
+		
+	},
 	// <--?end-->
 	loadWork:function(data) {
 		var toLoad;
-		trace(data);
 		for(var i in data) {
-			// trace(i);
 			toLoad = data[i];
-			// trace(toLoad);
 			
 			if (toLoad.thumb){
-				addTile(toLoad.head, toLoad, toLoad.format);
+				addTile(toLoad.head, toLoad, toLoad.addTo);
+				// trace(toLoad);
 			}
 		}
 
+
 		function addTile(name, content, addTo) {
+
 			$thumb = $('<img>').addClass('img').attr('src', content.thumb);
 			
 			$figCaption = $('<figcaption>').addClass(name);
@@ -177,7 +216,20 @@ var fn = {
 			$figure = $('<figure>').append($thumb,$figCaption);;
 
 			$tile = $('<div>').addClass('tile').data('content',content).append($figure);
-			// trace(addTo);	
+
+			if (content.special){
+				trace(name);
+				
+				// this needs a way to update the dom
+				// .attr('xlink:href', '/svg/svg-defs.svg#lock-icon')
+				// $lockedIcon = $('<use>').addClass('lock-icon');
+				// $locked = $('<svg>').addClass('icon').append($lockedIcon);
+
+				// $('.socialLinks').append($locked);
+				$tile.addClass('locked');
+
+			}
+
 			switch (addTo) {
 				case 'project':
 					$tile.addClass('three-col');
@@ -187,14 +239,49 @@ var fn = {
 					$tile.addClass('two-col');
 					$('#skillTiles').append($tile);
 					break;
-				case 'modal':
+				case 'section':
 					$tile.addClass('three-col');
 					$('#section-tiles').append($tile);
 					break;
 			}
 		}
+		// trace($('.lock-icon'));
+		// $('.lock-icon').setAttributeNS( 'http://www.w3.org/1999/xlink', 'href', '/svg/svg-defs.svg#lock-icon' );
 	},
 
+	loadRichContent: function(item){
+		if (item.flash){
+			$.each(item.flash, function(index,section){
+				var myColours = ['#58c8f3','#e55c95','#eed652'];				
+				$section = $('<div>').addClass('clearfix banner-holder ' + index);
+				$.each(section, function(index,content){				
+					var colour = randomColour(myColours);
+					myColours = $.grep(myColours, function(n) {
+					  return n != colour;
+					});
+
+					$img = $('<img>').addClass('replace-me').attr('src', content.backup);
+					$flashBanner = $('<div>').addClass('banner').data('content',content).append($img);
+					$container = $('<div>').addClass('block container ' + index).css('background-color', colour).append($flashBanner);
+					$section.append($container);
+				});
+				$('#flash-work').append($section);
+			});
+		}
+		if (item.video){
+			var Height = 9*($('#video-work').width()/16);
+			$.each(item.video, function(index,section){
+				$vimeoVid = $('<iframe>').addClass('vimeoVid').attr('src',section.src).css('height', Height);
+			});
+			$('#video-work').append($vimeoVid);
+
+			$( window ).resize(function() {
+				Height = 9*($('#video-work').width()/16);
+			  $('.vimeoVid').css('height', Height);
+			});
+		}
+	},
+	
 	
 // >>>>>>>>>>>>> formatting for pages <<<<<<<<<<<<<<<<
 
@@ -210,32 +297,32 @@ var fn = {
 							markup += '<div class="hero-image"></div>';
 							break;
 						case 'section 1':
-							markup += '<div id="projectSection" class="tileSection clearfix">'
-							markup += '<div class="case-studies"><h1>' + section.head +'</h1><p>' + section.copy + '</p></div>';
-							markup += '<div id="projectTiles" class="tiles three-col"></div></div>';
+							markup += '<div id="projectSection" class="tileSection clearfix"><div class="doubleCol"><h1>' + section.head +'</h1><p>' + section.copy + '</p></div>'
+							markup += '<div id="projectTiles" class="tileSection skill-copy tiles three-col"></div></div>'
 							break;
 						case 'section 2':
 							markup += '<div class="sectionHolder"><div id="skillSection" class="tileSection clearfix"><div class="singleCol"><h1>' + section.head +'</h1><p>' + section.copy + '</p></div>'
 							markup += '<div id="skillTiles" class="tiles two-col"></div></div></div>'
 							break;
 						case 'section 3':
-							markup += '<div id="aboutMe" class="tileSection clearfix"><div class=floatHolder><div class="doubleCol doubleImage"><img src=' + section.images + '></img></div><div class="singleCol about-me"><h1>' + section.head +'</h1><p>' + section.copy + '</p></div></div></div>'
+							markup += '<div id="aboutMe" class="tileSection clearfix"><div class="doubleCol doubleImage"><img src=' + section.images + '></img></div><div class="singleCol about-me"><h1>' + section.head +'</h1><p>' + section.copy + '</p></div>'
+							markup += '<button class="email" href="mailto:alex@franklynonsense.com?subject=Your site is frankly nonsense! I wanted to say hello because..."><h1>send me an email</h1></button></div></div>'
 							break;
 					}
 				});
 				break;
-			case 'caseStudy':
+			case 'project':
+
 				$.each(item.main, function (index, section){
 					switch (index){
 						case 'hero':
-							markup += '<div class="tileSection"><div class="doubleCol"><h1>' + section.head + '</h1>' + '<p>' + section.copy + '</p></div></div>'
+							markup += '<div class="tileSection skill-copy clearfix"><div class="doubleCol"><h1>' + section.head + '</h1>' + '<p>' + section.copy + '</p></div></div>'
 							break;
-						case 'section 1':
-							// append copy to markup
-							markup += '<div class="sectionHolder"><div class="tileSection"><div class="singleCol"><h1>' + section.head +'</h1><p>' + section.copy + '</p></div></div></div>';
+							
+						case 'thumbnailImages':
+							markup += '<div class="tileSection clearfix"><div id="section-tiles" class="tiles three-col"></div></div>';
+							// markup += '<div class="modal hide"></div>';
 							break;
-						// case "section 2":
-						// 	break;
 					}
 				});
 				break;
@@ -254,28 +341,30 @@ var fn = {
 				});
 				break;
 			case 'modal':
-				markup += '<div class="modal-content"><div class="tileSection clearfix"><div class="doubleCol"><h1>' + item.head + '</h1><h2>created for <a target="_blank" href='+item.subHeadLink+'>' + item.subHead + '</a></h2><p>' + item.copy + '</p></div>'
-				markup += '<div class="addthis_toolbox addthis_32x32_style"><a class="addthis_button_facebook social-btn" style="cursor:pointer"></a><a class="addthis_button_twitter social-btn" style="cursor:pointer"></a><a class="addthis_button_linkedin social-btn" style="cursor:pointer"></a><a class="addthis_button_pinterest_share social-btn" style="cursor:pointer"></a><a class="addthis_button_email social-btn" style="cursor:pointer"></a></div></div>';
-				// trace(item.images);
+				markup += '<div class="modal-content"><h1 class="close">&times</h1><div class="tileSection clearfix"><div class="doubleCol"><h1>' + item.head + '</h1><h2>created for <a target="_blank" href='+item.subHeadLink+'>' + item.subHead + '</a></h2><p>' + item.copy + '</p></div>'
+				markup += '<div class="addthis_toolbox addthis_32x32_style"><a class="addthis_button_facebook social-btn" style="cursor:pointer"></a><a class="addthis_button_twitter social-btn" style="cursor:pointer"></a><a class="addthis_button_linkedin social-btn" style="cursor:pointer"></a><a class="addthis_button_tumblr social-btn" style="cursor:pointer"></a><a class="addthis_button_pinterest_share social-btn" style="cursor:pointer"></a></div></div>';
 				if (item.images.length > 0){
-					trace(item.images);
 					markup += '<div class="modal-images">'
 					$.each(item.images, function(index,image){
-						trace(image);
 						markup += '<img src=' + image + '></img>'
 					})
 					markup += '</div>'
 				}
 
+				if (item.flash){
+					markup += '<div id="flash-work"></div>';
+				}
+				if (item.video){
+					markup += '<div id="video-work"></div>';
+				}
 				markup += '</div>';
 				break;
+			case '404':
+			trace('loading 404');
+				markup += '<div class="tileSection skill-copy clearfix"><div class="doubleCol"><h1>' + section.head + '</h1>' + '<p>' + section.copy + '</p></div></div>'
 		}
 		return markup;
 	},
-
-	loadModalContent: function(item) {
-
-	}
 }
 
 function validateEmail(email) {
@@ -287,12 +376,18 @@ function validateText(text) {
 	return re.test(text);
 }
 
-function cleanURL(url) {
-	var exp = window.location.href.split(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/);
+function cleanURL(a) {
+	var exp = a.split(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/);
 	var url = exp[4] + exp[5];
-	var splitUrl = url.split('/');
-	// trace (splitUrl);
+	var b = url.split(fn.hostUrl).join('');
+	var splitUrl = b.split('/');
+
 	return splitUrl;
+}
+
+function randomColour(colours){
+	
+	return colours[Math.floor(Math.random() * colours.length)];
 }
 
 
@@ -304,19 +399,6 @@ if ('console' in self && 'log' in console) console.log(s);
 
 //Ready function, load content
 $(document).ready(function () {
-
-		//needs a timer
-
-
 	    addthis.init(); //callback function for script loading
 		$.getJSON('content.json', fn.init);
-
-
-		// $('.tile').hide();
-		
-
-		// 	setTimeout(function(){
-		// 		//show one at a time at random scale from 0 to
-		// 		$('.tile').show();
-		// 	},500);
 });
